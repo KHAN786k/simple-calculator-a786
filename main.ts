ba352c114f81c8318b26fa8bdce67e0c97a355fb
Abdul Razzaq <razzaqkict@gmail.com>
2024-06-02 11:19:46 +0500

    push my simple calculator on github

---
+++ main.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+const answer=await inquirer.prompt([
+    {
+        type:"number",
+        name:"num1",
+        message: "please enter your first number."
+    },
+    {
+        type:"number",
+        name:"num2",
+        message: "please enter your second number."
+    },
+    {
+        type:"list",
+        name:"operator",
+        message: "please sellect one of the operator to perform operation.",
+        choices:['add','sub','div','mult']
+    },
+])
+if (answer.operator==="add"){
+    console.log(`your result is:`, answer.num1 + answer.num2)
+}
+else if (answer.operator==="sub"){
+    console.log(`your result is:`, answer.num1 - answer.num2)
+}
+else if (answer.operator==="div"){
+    console.log(`your result is:`, answer.num1 / answer.num2)
+}
+else if (answer.operator==="mult"){
+    console.log(`your result is:`, answer.num1 * answer.num2)
+}
+else{
+    console.log("please enter valid operator.")
+}
